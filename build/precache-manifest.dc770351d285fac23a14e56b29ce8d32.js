6ff3c530e1adcd2ce8bfb395706967388c92a116
anthony ancel <aancel@MacBook-Air-de-anthony.local>
2019-05-21 18:07:55 +0200

    build

---
+++ build/precache-manifest.dc770351d285fac23a14e56b29ce8d32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5829fdbc53875a475a09eeaa852a622",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14702225294b4f3b1fdf",
+    "url": "/static/css/main.9cec1c4a.chunk.css"
+  },
+  {
+    "revision": "0b33e78f82adea42860c",
+    "url": "/static/js/2.121fbf27.chunk.js"
+  },
+  {
+    "revision": "14702225294b4f3b1fdf",
+    "url": "/static/js/main.9095e043.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
